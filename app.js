37c4222cc90426fe90547c6a278f98a33f1432aa
Manisharora8600 <43010755+Manisharora8600@users.noreply.github.com>
2021-10-03 15:51:12 +0530

    Added error case Feature

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ function getjokes(e) {
                 response.value.forEach(function(joke) {
                     output += `<li>${joke.joke}</li>`;
                 });
-            } 
+            } else {
+                output+= '<li>Something went Wrong!!!</li>';
+            }
 
             document.querySelector('.jokes').innerHTML = output;
         }
